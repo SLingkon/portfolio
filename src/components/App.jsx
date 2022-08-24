e087d3a4b1bb7430fe6d0f48b1b87c82dff18f59
LingkonLap <lingkon244@gmail.com>
2022-08-24 16:25:02 +0600

    last commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProfilePic from './ProfilePic';
+import Info from './Info';
+import Nav from './Nav';
+import Project from './Project';
+function App() {
+	return (
+		<>
+			<ProfilePic />
+			<Info />
+			<Nav />
+			<Project />
+		</>
+	);
+}
+export default App;
